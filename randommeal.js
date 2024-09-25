c9d14d97b1fcf10a87cc57f33cc8d986b8475912
Naiya-19 <122500623+Naiya-19@users.noreply.github.com>
2024-09-25 18:42:52 -0500

    Add files via upload

---
+++ randommeal.js
@@ -0,0 +1,87 @@
+const entree1 = {
+
+    starter : "Bacon Cheese Fries",
+    
+    main: "Beef Burger and Coleslaw",
+    
+    dessert: "Deep Fried Ice-Cream"
+    
+    };
+    
+    
+    
+    const entree2 = {
+    
+    starter : "Smoked Salmon On Rye",
+    
+    main: "Vegetarian Couscous Rolls",
+    
+    dessert: "Fruit Plate"
+    
+    };
+    
+    
+    
+    const entree3 = {
+    
+    starter : "Chips and Hummus",
+    
+    main: "Grilled Chicken in Hot Sauce",
+    
+    dessert: "Chocolate Ganache Cake"
+    
+    };
+    
+    const entree4 = {
+    
+    starter : "Nachos",
+    
+    main: "Chicken Sandwich",
+    
+    dessert: "Cookie"
+    
+    };
+    
+    const entree5 = {
+    
+    starter : "Boneless Chicken",
+    
+    main: "Steak",
+    
+    dessert: "Cookie Dough Ice Cream"
+    
+    };
+    
+    // ... add more meal objects
+    
+    // Add more choices and update the array
+    
+    const meals = [entree1, entree2, entree3, entree4, entree5];
+    
+    
+    
+    const randomMeal = function() {
+    
+    // Get a random number between 0 and the length of the array, meals
+    
+    let mealNumber = Math.floor(Math.random() * meals.length);
+    
+    showMeal(mealNumber);
+    
+    }
+    
+    
+    const showMeal = (mealNumber)=> {
+    
+    let str = `Start with ${meals[mealNumber].starter}, 
+    
+    followed by ${meals[mealNumber].main}, 
+    
+    and finish off your meal with ${meals[mealNumber].dessert}`;
+    
+    
+    
+   console.log(str);
+    
+    };
+    
